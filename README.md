30df6e958c4b09fba65920ce522e10a6945e68ac
EzMinh <64187662+EzMinh@users.noreply.github.com>
2020-05-12 11:09:00 +0700

    Upload Files
    Upload Main.php, plugin.yml and README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+[![HitCount](http://hits.dwyl.com/EzMinh/Bin.svg)](http://hits.dwyl.com/EzMinh/Bin)
+![GitHub All Releases](https://img.shields.io/github/downloads/EzMinh/Bin/total)
+![GitHub](https://img.shields.io/github/license/EzMinh/Bin)
+# Bin 1.0.0
+**Remove items in your inventory**
+# How to use
+- Use /bin help
+# Permission
+| Permission |        Description        | Default |
+|:----------:|:-------------------------:|:-------:|
+|   bin.cmd  | Allows player to use /bin |   True  |
+# License and Copyright
+[LGPL-3.0](https://github.com/EzMinh/Bin/blob/1.0.0/LICENSE) <br/>
+© 2020 EzMinh
---
+++ src/EzMinh/Bin/Main.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace EzMinh\Bin;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\plugin\PluginBase;
+use pocketmine\inventory\Inventory;
+
+class Main extends PluginBase 
+{
+    public function onEnable()
+    {
+        $this->getLogger()->info(C::GREEN . "Enabled" . C::YELLOW . " Bin");
+    }
+    public function onDisable()
+    {
+        $this->getLogger()->info(C::RED . "Disabled" . C::YELLOW . " Bin");
+    }
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool
+    {
+        if($cmd->getName() == "bin")
+        {
+            if(!$sender instanceof Player)
+            {
+                $sender->sendMessage(C::RED . "You can't not use this command here!");
+                return false;
+            }
+            if(!$sender->hasPermission("bin.cmd"))
+            {
+                $sender->sendMessage(C::RED . "You don't have permission to use this command!");
+                return false;
+            }
+            if(count($args) == 0)
+            {
+                $sender->sendMessage("Usage: /bin help");
+            } else {
+                if(count($args) == 1)
+                    {
+                        switch($args[0])
+                        {
+                            case "help":
+                                $sender->sendMessage("Usage: /bin <value>\n /bin hand to remove all items in your hand\n /bin inv to remove all items in your inventory\n ");
+                            break;
+                            case "hand":
+                                $item = $sender->getInventory()->getItemInHand();
+                                $sender->getInventory()->removeItem($item);
+                                $sender->sendMessage(C::GREEN . "Success!");
+                            break;
+                            case "inv":
+                                $inv = $sender->getInventory()->getContents();
+                                foreach($inv as $item)
+                                {
+                                    $sender->getInventory()->remove($item);
+                                }
+                                $sender->sendMessage(C::GREEN . "Success!");
+                            break;
+                        }
+                    }
+            }
+        }
+        return true;
+    }
+}
